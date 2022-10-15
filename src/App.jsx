8c525bd982a67eb844bb1d12ca933730ee8d5a18
Vinícius <viniscrv@gmail.com>
2022-10-15 16:45:40 -0300

    upload arquivos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Banner from './components/Banner'
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <header className='banner'>
+        <img src='/imagens/banner.png' alt='Banner'/>
+    </header>
+  )
+}
+
+export default Banner
