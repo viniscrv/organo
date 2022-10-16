ecfba37de507adaf5dc0440cddef80b582e6fd1e
Vinícius <viniscrv@gmail.com>
2022-10-16 01:32:36 -0300

    finalizando formulario estático

---
+++ src/components/Botao/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Botao.css'
+
+const Botao = (props) => {
+  return <button>{props.children}</button>
+}
+
+export default Botao
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,16 +1,37 @@
 import React from "react";
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 
 const Formulario = () => {
+
+  const grupos = [
+    'grupo 1',
+    'grupo 2',
+    'grupo 3',
+    'grupo 4',
+    'grupo 5',
+  ]
+
+  const aoSalvar = (event) => {
+    event.preventDefault()
+    alert('ok')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigario={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigario={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa
+        label='Time'
+        itens={grupos}
+        />
+        <Botao>Click me</Botao>
       </form>
     </section>
   );
